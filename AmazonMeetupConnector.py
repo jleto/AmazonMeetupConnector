9f8ef336a829df84c48190be0675a609e101c649
Justin Leto <leto.justin@gmail.com>
2014-01-12 17:44:19 -0500

    Added support for passing parameters in from the command line to meetup connector.

--- AmazonMeetupConnector.py
+++ AmazonMeetupConnector.py
@@ -25,10 +25,15 @@ with open('meetup.properties', 'r') as f:
 
 #   If only utility script is called
 if len(sys.argv) <= 1:
-    sys.exit("Usage: python %s <options> <csvfiles>\n"
+    sys.exit("Usage: python %s [OPTION] [PARAMETERS]\n"
              "Where possible options include:\n"
-             "   --merge            Merge amazon and meetup payment information.\n"
-	         "   --help             Help for using this tool." % sys.argv[0]
+             "   --merge                        Merge amazon and meetup payment information.\n"
+             "   --help                         Help for using this tool.\n"
+             "Where possible optional parameters include:\n"
+             "   --start-date=STARTDATE         Start date of the search. If omitted, search will start at the first transaction.\n"
+             "   --end-date=ENDDATE             End end of the search. If ommitted, search will span to the most current transaction.\n"
+			 "   --meetup-groupname=GROUPNAME   Name of the Meetup Group to query.\n"
+			 "   --meetup-apikey=APIKEY         Meetup Group Api Key.\n"
 			 )
 
 #   If help is requested
@@ -37,9 +42,28 @@ elif (sys.argv[1] == '--help'):
 
 #   Test for valid options, instantiate provider objects
 if sys.argv[1] == '--merge':
-    meetupProvider = meetup.meetup(meetupProperties['groupName'], meetupProperties['apiKey'])
-    startDate = datetime.strptime(sys.argv[2], '%m/%d/%Y')
-    endDate = datetime.strptime(sys.argv[3], '%m/%d/%Y')
+
+    meetupGroupName = meetupProperties['groupName']
+    meetupApiKey = meetupProperties['apiKey']
+    startDate = None
+    endDate = None
+    
+    #   If user passes parameters in as arguments, override properties file
+    for arg in sys.argv[2:]:
+        if arg.split("=",1)[0] == "--start-date":
+            startDate = datetime.strptime(arg.split("=",1)[1], '%m/%d/%Y')
+        elif arg.split("=",1)[0] == "--end-date":
+            endDate = datetime.strptime(arg.split("=",1)[1], '%m/%d/%Y')
+        elif arg.split("=",1)[0] == "--meetup-groupname":
+            meetupGroupName = arg.split("=",1)[1]
+        elif arg.split("=",1)[0] == "--meetup-apikey":
+            meetupApiKey = arg.split("=",1)[1]
+
+    #   Instantiate meetup object with group name and api key
+    meetupProvider = meetup.meetup(meetupGroupName, meetupApiKey)
+
+	#   Get payment transactions from meetup for the defined time period
     payments = meetupProvider.getPayments(startDate, endDate)
+
     pprint (payments) 
 
